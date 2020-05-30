6a0491294239ee22025d213c4ed2426248356f74
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-30 22:54:14 +0200

    baseUrl

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./css/index.css";
 import { Provider } from "react-redux";
 import configureStore from "./state/store/configureStore";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://dailynewssense-api.herokuapp.com/api";
 const store = configureStore();
 window.store = store
 ReactDOM.render(
